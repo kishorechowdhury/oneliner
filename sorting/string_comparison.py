24e1903cb68bc804e1261d85901ce7a74714d121
Kishore Chowdhury <kishore.chowdhuri@gmail.com>
2024-05-20 12:40:33 +0530

    3rd push

--- sorting/string_comparison.py
+++ sorting/string_comparison.py
@@ -1,2 +1,8 @@
 text = 'A is a but B is not a'
 print(sorted(text.split(), key=str.casefold))
+
+
+'''
+output:
+['A', 'a', 'a', 'B', 'but', 'is', 'is', 'not']
+'''
--- sorting/tuple_sort.py
+++ sorting/tuple_sort.py
@@ -4,3 +4,9 @@ student_tuples = [
     ('ajay', 'B', 15),
 ]
 print(sorted(student_tuples, key=lambda student: student[2]))
+
+
+'''
+output:
+[('ajay', 'B', 15), ('sanjay', 'A', 16), ('vijay', 'B', 17)]
+'''
